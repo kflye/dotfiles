7f647c36aa123a64556fdf7352931f42502b7c3b
Kasper Flye <klf@sekoia.dk>
2023-04-27 16:13:16 +0200

    show dot(.) files in neo-tree

--- nvim/after/plugin/neo-tree.lua
+++ nvim/after/plugin/neo-tree.lua
@@ -37,6 +37,20 @@ neotree.setup({
                 neotree.close_all()
             end
         },
+    },
+    filesystem = {
+        filtered_items = {
+            visible = true,
+            show_hidden_count = true,
+            hide_dotfiles = false,
+            hide_gitignored = true,
+            hide_by_name = {
+                -- '.git',
+                -- '.DS_Store',
+                -- 'thumbs.db',
+            },
+            never_show = {},
+        },
     }
 })
 
