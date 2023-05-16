04d7abb2c81106cb2b1882b68de9715415d203c5
Kasper Flye <klf@sekoia.dk>
2023-05-16 08:35:16 +0200

    add rust code for rust book

---
+++ testfiles/rust/guessing_game/src/main.rs
@@ -0,0 +1,23 @@
+use rand::Rng;
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is: {secret_number}");
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        std::cmp::Ordering::Less => println!("Too small!"),
+        std::cmp::Ordering::Greater => println!("Too big!"),
+        std::cmp::Ordering::Equal => println!("You win!"),
+    }
+}
