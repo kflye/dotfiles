e45946094b02f3cc39af8172ace060f008ae0b3f
Kasper Flye <kasperflye@gmail.com>
2023-11-17 14:48:40 +0100

    linux setup

--- fish/setup.sh
+++ fish/setup.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+dir=$(dirname "$(realpath $0)");
 
-curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher   
+ln -s "$dir/config.fish" ~/.config/fish/config.fish
+
 
 wget https://gitlab.com/kyb/fish_ssh_agent/raw/master/functions/fish_ssh_agent.fish -P ~/.config/fish/functions/
 
+# run these in a fish shell
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher   
+
 # Theme
 fisher install jomik/fish-gruvbox
+
+# 
+fisher install jethrokuan/fzf
+
--- setup.sh
+++ setup.sh
@@ -1,11 +1,35 @@
 #!/usr/bin/env fish
+#
+# fractional scaling gnome :  gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffer']" 
 
 if [[ -z "$XDG_CONFIG_HOME" ]]; then
 	echo $XDG_CONFIG_HOME
 	echo ´XDG_CONFIG_HOME not set´
-	exit 1
+	# exit 1
 fi
 
 echo $XDG_CONFIG_HOME
 
-sudo apt install fish ripgrep fd-find fzf zoxide python3-pip
+# sudo apt install ripgrep fd-find fzf zoxide python3-pip
+
+clone_path="${clone_path:-"${PWD}"}"
+echo "Current working directory: $clone_path"
+
+rm -rf ~/.config/nvim;
+ln -fs "${clone_path}/nvim" "${HOME}/.config/nvim"
+
+
+ln -fs "${clone_path}/starship/starship.toml" "${HOME}/.config/starship.toml"
+
+# git
+ln -fs "${clone_path}/git/.gitconfig" "${HOME}/.gitconfig"
+ln -fs "${clone_path}/git/.gitignore" "${HOME}/.gitignore"
+ln -fs "${clone_path}/git/.gitattributes" "${HOME}/.gitattributes"
+
+cp "${clone_path}/git/.gitconfig.local" "${HOME}/.gitconfig.local" 
+
+# New-Item -ItemType SymbolicLink -Force -Path $HOME/.gitconfig -Target $path\.gitconfig;
+# New-Item -ItemType SymbolicLink -Force -Path $HOME/.gitignore -Target $path\.gitignore
+# New-Item -ItemType SymbolicLink -Force -Path $HOME/.gitattributes -Target $path\.gitattributes
+
+
