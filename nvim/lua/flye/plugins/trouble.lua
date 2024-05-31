49067dc3b650abfc85de65862d30d84ae52f513e
Kasper Flye <klf@bankdata.dk>
2024-05-31 15:54:40 +0200

    update trouble

--- nvim/lua/flye/plugins/trouble.lua
+++ nvim/lua/flye/plugins/trouble.lua
@@ -1,14 +1,39 @@
 -- A pretty list for showing diagnostics, references, telescope results, quickfix and location lists
 
-return {
-  "folke/trouble.nvim",
-  dependencies = "nvim-tree/nvim-web-devicons",
-  keys = {
-    {"<leader>xx", "<cmd>TroubleToggle<cr>"},
-    {"<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>"},
-    {"<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>"},
-    {"<leader>xl", "<cmd>TroubleToggle loclist<cr>"},
-    {"<leader>xq", "<cmd>TroubleToggle quickfix<cr>"},
-    {"<cmd>TroubleToggle lsp_references<cr>"},
-  }
+return
+{
+    "folke/trouble.nvim",
+    keys = {
+        {
+            "<leader>xx",
+            "<cmd>Trouble diagnostics toggle<cr>",
+            desc = "Diagnostics (Trouble)",
+        },
+        {
+            "<leader>xX",
+            "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+            desc = "Buffer Diagnostics (Trouble)",
+        },
+        {
+            "<leader>cs",
+            "<cmd>Trouble symbols toggle focus=false<cr>",
+            desc = "Symbols (Trouble)",
+        },
+        {
+            "<leader>cl",
+            "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+            desc = "LSP Definitions / references / ... (Trouble)",
+        },
+        {
+            "<leader>xL",
+            "<cmd>Trouble loclist toggle<cr>",
+            desc = "Location List (Trouble)",
+        },
+        {
+            "<leader>xQ",
+            "<cmd>Trouble qflist toggle<cr>",
+            desc = "Quickfix List (Trouble)",
+        },
+    },
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
 }
--- setupCorePrograms.ps1
+++ setupCorePrograms.ps1
@@ -1,31 +1,25 @@
-choco install firacodenf nerd-fonts-jetbrainsmono `
-  zig `
-  mingw `
-  make `
-  -y
-
-# zig -> c++ compiler for nvim-treesitter parsers
-
-winget install --id=Git.Git -e  ;
-winget install --id=jftuga.less -e ;
-winget install --id=dandavison.delta -e ;
-winget install --id=Microsoft.VisualStudioCode -e  ; 
 winget install --id=Docker.DockerDesktop -e  ; 
-winget install --id=JetBrains.Toolbox -e  ; 
-winget install --id=CoreyButler.NVMforWindows -e  ; # nvm for nodejs, 'nvm install 18.14.2 -> nvm use 18.14.2
-winget install --id=Postman.Postman -e  ; 
+# winget install --id=JetBrains.Toolbox -e  ; 
+# winget install --id=Postman.Postman -e  ; 
+# winget install --id=QMK.QMKToolbox -e  ; 
 
-winget install --id=ajeetdsouza.zoxide -e  ; 
-winget install --id=Starship.Starship -e  ;
 
-winget install --id=Microsoft.PowerToys -e  ; 
-winget install --id=Obsidian.Obsidian -e  ; 
-winget install --id=Bitwarden.Bitwarden -e ;
+# winget install --id=Git.Git -e  ;
+# winget install --id=jftuga.less -e ;
+# winget install --id=dandavison.delta -e ;
+# winget install --id=Microsoft.VisualStudioCode -e  ; 
+# winget install --id=CoreyButler.NVMforWindows -e  ; # nvm for nodejs, 'nvm install 18.14.2 -> nvm use 18.14.2
 
-winget install --id=SamHocevar.WinCompose -e ;
-winget install --id=QMK.QMKToolbox -e  ; 
-winget install --id=Spotify.Spotify -e  ; 
-winget install --id=7zip.7zip -e  ;
+# winget install --id=ajeetdsouza.zoxide -e  ; 
+# winget install --id=Starship.Starship -e  ;
+
+# winget install --id=Microsoft.PowerToys -e  ; 
+# winget install --id=Obsidian.Obsidian -e  ; 
+# winget install --id=Bitwarden.Bitwarden -e ;
+
+# winget install --id=SamHocevar.WinCompose -e ;
+# winget install --id=Spotify.Spotify -e  ; 
+# winget install --id=7zip.7zip -e  ;
 
 
 
@@ -36,13 +30,6 @@ winget install --id=7zip.7zip -e  ;
 
 # https://github.com/sainnhe/gruvbox-material/wiki/Related-Projects
 
-## scoop
-winget install --id=Neovim.Neovim -e  ; 
-winget install --id=junegunn.fzf -e  ; 
-winget install --id=sharkdp.fd -e  ; 
-winget install --id=BurntSushi.ripgrep.GNU -e  ; 
-winget install --id=sharkdp.bat -e  ; 
-winget install --id=gsass1.NTop -e ;
 
 scoop bucket add nerd-fonts
 scoop install nerd-fonts/JetBrainsMono-NF
@@ -53,16 +40,37 @@ scoop bucket add extras
 scoop bucket add versions
 
 scoop install vcredist2022
-scoop install versions/neovim-nightly
-scoop install main/neovim
-scoop install main/fzf
-scoop install main/fd
-scoop install main/ripgrep
+
+
+scoop install main/7zip
+scoop install main/ntop
+scoop install extras/lazygit
+scoop install extras/wincompose
+scoop install extras/powertoys
+scoop install extras/bitwarden
+scoop install extras/obsidian
+scoop install extras/spotify
+scoop install extras/vscode
+scoop install extras/qmk-toolbox
+scoop install extras/postman
+scoop install extras/jetbrains-toolbox
+
+
+scoop install main/git
 scoop install main/make
 scoop install main/gcc
+scoop install main/nvm
+
+
+scoop install main/starship
+scoop install main/zoxide
 scoop install main/bat
-scoop install main/ntop
-scoop install main/nodejs
+scoop install main/fd
+scoop install main/fzf
+scoop install main/ripgrep
 scoop install main/delta
 scoop install main/less
-scoop install extras/lazygit
+
+
+scoop install main/neovim
+scoop install main/dark
