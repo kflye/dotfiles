ea4b76bcebf59addb1bd165c2f4748eb26ba9cb0
Kasper Flye <klf@bankdata.dk>
2024-08-08 07:50:26 +0200

    fix create augroup

--- nvim/.config/nvim/lua/flye/plugins/lsp/lsp.lua
+++ nvim/.config/nvim/lua/flye/plugins/lsp/lsp.lua
@@ -90,6 +90,11 @@ vim.api.nvim_create_autocmd('LspAttach', {
         end, 'Format current buffer with LSP', bufnr)
 
         if client and client.server_capabilities.documentHighlightProvider then
+            -- The following two autocommands are used to highlight references of the
+            -- word under your cursor when your cursor rests there for a little while.
+            --    See `:help CursorHold` for information about when this is executed
+            --
+            -- When you move your cursor, the highlights will be cleared (the second autocommand).
             local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
             vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
                 buffer = event.buf,
@@ -101,6 +106,14 @@ vim.api.nvim_create_autocmd('LspAttach', {
                 buffer = event.buf,
                 group = highlight_augroup,
                 callback = vim.lsp.buf.clear_references,
+            })
+
+            vim.api.nvim_create_autocmd('LspDetach', {
+                group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+                callback = function(event2)
+                    vim.lsp.buf.clear_references()
+                    vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
+                end,
             })
         end
 
@@ -115,14 +128,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
             end, '[C]ode toggle Inlay [H]ints')
         end
     end
-})
-
-vim.api.nvim_create_autocmd('LspDetach', {
-    group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
-    callback = function(event)
-        vim.lsp.buf.clear_references()
-        vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event.buf }
-    end,
 })
 
 vim.keymap.set('n', '<leader>ce', vim.diagnostic.open_float)
