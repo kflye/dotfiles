c4b00a03de60081d059eda923c3dd1acc9f619a5
Kasper Flye <klf@bankdata.dk>
2024-08-23 14:05:05 +0200

    use num2str/navigator, for tmux/wezterm/zellij navigation

---
+++ nvim/.config/nvim/lua/flye/plugins/editor/navigator.lua
@@ -0,0 +1,16 @@
+return {
+    -- fork with zellij suppert - https://github.com/dynamotn/Navigator.nvim
+    -- https://github.com/alex35mil/dotfiles
+    -- https://github.com/hiasr/vim-zellij-navigator
+    -- https://www.reddit.com/r/zellij/comments/1dupuxv/ctrl_hjkl_hassles/
+    "numToStr/Navigator.nvim",
+    config = function()
+        vim.keymap.set({ 'n', 't' }, '<C-h>', '<CMD>NavigatorLeft<CR>')
+        vim.keymap.set({ 'n', 't' }, '<C-l>', '<CMD>NavigatorRight<CR>')
+        vim.keymap.set({ 'n', 't' }, '<C-k>', '<CMD>NavigatorUp<CR>')
+        vim.keymap.set({ 'n', 't' }, '<C-j>', '<CMD>NavigatorDown<CR>')
+        vim.keymap.set({ 'n', 't' }, '<C-p>', '<CMD>NavigatorPrevious<CR>')
+
+        require('Navigator').setup()
+    end,
+}
