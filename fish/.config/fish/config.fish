b671ee836c768c5167d5a3be9557f77e0f83f15b
Kasper Flye <klf@bankdata.dk>
2025-07-04 08:44:33 +0200

    fish config cleanup, add zsh initial config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,7 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     fish_ssh_agent
     starship init fish | source
-    fzf --fish | source
     zoxide init fish | source
     fzf --fish | source
 
@@ -10,13 +9,13 @@ if status is-interactive
     # fish_config theme choose "tokyonight_storm"
     # theme_gruvbox dark medium
 
-    set -l SSH_KEYS ~/.ssh/id_rsa ~/.ssh/id_ed25519
-    for FILE in $SSH_KEYS
-        if test -e $FILE
-            set cmd keychain --eval --agents ssh $FILE
-        end
-    end 
-    eval $cmd
+    # set -l SSH_KEYS ~/.ssh/id_rsa ~/.ssh/id_ed25519
+    # for FILE in $SSH_KEYS
+    #     if test -e $FILE
+    #         set cmd keychain --eval --agents ssh $FILE
+    #     end
+    # end 
+    # eval $cmd
     
     if test -e "/usr/sbin/wsl2-ssh-agent"
         eval "$(/usr/sbin/wsl2-ssh-agent)"
@@ -26,11 +25,10 @@ if status is-interactive
     # fish_user_key_bindings
 
     # Enable fzf key bindings
-    set -u FZF_DEFAULT_COMMAND "fd --type f --color=never --hidden"
-    set -U FZF_CTRL_T_COMMAND "fd --type f --color=never --hidden"
-    set -U FZF_CTRL_T_OPTS "--preview 'bat --color=always --line-range :50 {}'"
-    set -U FZF_ALT_C_COMMAND "fd --type d --color=never --hidden"
-    fzf_key_bindings
+    # set -u FZF_DEFAULT_COMMAND "fd --type f --color=never --hidden"
+    # set -U FZF_CTRL_T_COMMAND "fd --type f --color=never --hidden"
+    # set -U FZF_CTRL_T_OPTS "--preview 'bat --color=always --line-range :50 {}'"
+    # set -U FZF_ALT_C_COMMAND "fd --type d --color=never --hidden"
 
     # opam configuration (keyboard generator)
     source $HOME/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
@@ -46,9 +44,8 @@ fish_add_path -a /usr/local/go/bin
 fish_add_path -a /usr/share/go/bin
 fish_add_path -a /usr/share/zig
 
-set -u VISUAL nvim
-set -u EDITOR "$VISUAL"
+set -gx VISUAL nvim
+set -gx EDITOR "$VISUAL"
 
-set -u XDG_CONFIG_HOME "$HOME/.config"
-set -x NODE_OPTIONS --max-old-space-size=8192
+set -gx XDG_CONFIG_HOME "$HOME/.config"
 set --universal nvm_default_version lts
