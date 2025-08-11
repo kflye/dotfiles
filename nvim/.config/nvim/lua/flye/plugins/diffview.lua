2b939c2a896db48dda1913641c5cf7cace4a914b
Kasper Flye <klf@bankdata.dk>
2025-08-11 15:28:42 +0200

    diffview shortcuts

--- nvim/.config/nvim/lua/flye/plugins/diffview.lua
+++ nvim/.config/nvim/lua/flye/plugins/diffview.lua
@@ -1,4 +1,14 @@
 return {
     "sindrets/diffview.nvim",
     opts = {},
+    keys = {
+        { '<leader>g',   '',                                           desc = '+git' },
+        { '<leader>gdf', "<Cmd>DiffviewFileHistory --follow %<CR>",    desc = '[G]it [D]iff [F]ile' },
+        { '<leader>gdd', "<Cmd>DiffviewOpen <CR>",                     desc = '[G]it [D]iff Open' },
+        { '<leader>gdc', "<Cmd>DiffviewClose <CR>",                     desc = '[G]it [D]iff Open' },
+
+        { '<leader>gdl', "<Cmd>'<,'>DiffviewFileHistory --follow<CR>", mode = { "v" },              desc = '[G]it [D]iff [L]ine(s) - visual selection' },
+        { '<leader>gdl', "<Cmd>.DiffviewFileHistory --follow<CR>",     mode = { "n" },              desc = '[G]it [D]iff [L]ine' },
+
+    }
 }
